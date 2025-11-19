acab34e1b457de6a134fda31019f873b27cdd1d8
David <davidcansecovelasco@gmail.com>
2025-11-18 18:24:33 -0600

    Version para actualizar

--- src/Controlador/Funciones.java
+++ src/Controlador/Funciones.java
@@ -14,25 +14,25 @@ public class Funciones extends Interfaz {
         }          
     }
     
-    public static void consultarU(String query, DefaultTableModel t){
-        java.sql.ResultSet r= new Controlador.Conectar().consulta(query); 
-        int i=1;
-        try{                      
-            while(r.next())
-            {
-                String x=r.getString("insumo")+" "+evaluarnull(r.getString("descripcion"))
-                        +" "+evaluarnull(r.getString("presentacion"));
-                id=r.getInt("no_item");
-                linea[0]=id;
-                linea[1]=x;
-                linea[3]=r.getString("total_de_piezas");
-                t.addRow(linea);   
-                i=i+1;
-            }
-        }catch(Exception ex) {
-            System.out.println(ex.getMessage());
-        }  
-    }
+//    public static void consultarU(String query, DefaultTableModel t){
+//        java.sql.ResultSet r= new Controlador.Conectar().consulta(query); 
+//        int i=1;
+//        try{                      
+//            while(r.next())
+//            {
+//                String x=r.getString("insumo")+" "+evaluarnull(r.getString("descripcion"))
+//                        +" "+evaluarnull(r.getString("presentacion"));
+//                id=r.getInt("no_item");
+//                linea[0]=id;
+//                linea[1]=x;
+//                linea[3]=r.getString("total_de_piezas");
+//                t.addRow(linea);   
+//                i=i+1;
+//            }
+//        }catch(Exception ex) {
+//            System.out.println(ex.getMessage());
+//        }  
+//    }
     
     private static String evaluarnull(String value) {
         if (value == null || value.trim().isEmpty()) {
@@ -41,16 +41,16 @@ public class Funciones extends Interfaz {
         return " - " + value;
     }
     
-    public static void buscando(String buscar){
-        Funciones.limpiaTabla(t1);        
-        if (!buscar.trim().isEmpty()) { 
-            Funciones.consultarU("SELECT * FROM inventario_dental WHERE "
-            + "insumo ILIKE '%" + buscar + "%' OR " // ILIKE para búsqueda sin distinción entre mayúsculas/minúsculas en PostgreSQL
-            + "descripcion ILIKE '%" + buscar + "%' OR "
-            + "CAST(no_item AS TEXT) LIKE '%" + buscar + "%' OR " // Convierte matrícula a texto para buscar coincidencias parciales
-            + "presentacion ILIKE '%" + buscar + "%'",t1);
-        }
-    }
+//    public static void buscando(String buscar){
+//        limpiaTabla(t1);        
+//        if (!buscar.trim().isEmpty()) { 
+//            consultarU("SELECT * FROM inventario_dental WHERE "
+//            + "insumo ILIKE '%" + buscar + "%' OR " // ILIKE para búsqueda sin distinción entre mayúsculas/minúsculas en PostgreSQL
+//            + "descripcion ILIKE '%" + buscar + "%' OR "
+//            + "CAST(no_item AS TEXT) LIKE '%" + buscar + "%' OR " // Convierte matrícula a texto para buscar coincidencias parciales
+//            + "presentacion ILIKE '%" + buscar + "%'",t1);
+//        }
+//    }
     
     
 }
